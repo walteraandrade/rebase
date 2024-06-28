4a647ffc877f54a9c0244ba13d7e7ec4e4bf0324
walteraandrade <walteraandrade@gmail.com>
2024-06-28 16:24:34 -0300

    card ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-
+      <div style={{ border: '1px solid black', padding: '8px', borderRadius: '20px'}}>
+        <h1>nome</h1>
+        <img src='imagem' />
+      </div>
     </>
   )
 }
